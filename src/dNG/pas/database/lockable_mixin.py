2ddf07f02c782dc287696df0ebd6fbdb6867765b
NotTheEvilOne <git-NTEO@vplace.de>
2014-07-23 21:20:13 +0200

    Enhancement: Added mixin for lockable entries

---
+++ src/dNG/pas/database/lockable_mixin.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+direct PAS
+Python Application Services
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+http://www.direct-netware.de/redirect.py?pas;database
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+http://www.direct-netware.de/redirect.py?licenses;mpl2
+----------------------------------------------------------------------------
+#echo(pasDatabaseOwnableVersion)#
+#echo(__FILEPATH__)#
+"""
+
+from .instance import Instance
+
+class LockableMixin(object):
+#
+	"""
+"LockableMixin" provides methods to handle lockable instances.
+
+:author:     direct Netware Group
+:copyright:  direct Netware Group - All rights reserved
+:package:    pas
+:subpackage: database
+:since:      v0.1.00
+:license:    http://www.direct-netware.de/redirect.py?licenses;mpl2
+             Mozilla Public License, v. 2.0
+	"""
+
+	is_locked = Instance._wrap_getter("locked")
+	"""
+Returns true if the entry is locked.
+
+:return: (bool) True if locked
+:since:  v0.1.00
+	"""
+#
+
+##j## EOF
